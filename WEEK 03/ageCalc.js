c27d7ca4cb4f6856ae66781f3830e758898808e2
Y0-0N63 <144354615+Y0-0N63@users.noreply.github.com>
2024-03-13 22:07:53 +0900

    Update ageCalc.js

--- WEEK 03/ageCalc.js
+++ WEEK 03/ageCalc.js
@@ -1,8 +1,8 @@
-function agecalc() {
-  var x = str,
-    result;
-  result = 21;
+function ageCalc() {
+  // 변수 result에 21 값 담기
+  var result = 21;
 
+  // 변수의 값(21)과 타입(정수)이 같으면
   if (result === 21) {
     var str = 'Correct, My age is ';
     alert(str + result);
