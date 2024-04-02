2d983035dca7959e9053c263c15f6d0ed08a422f
최보윤 <144354615+Y0-0N63@users.noreply.github.com>
2024-04-02 11:30:00 +0900

    Create addOddNum.js

---
+++ Week 06/addOddNum.js
@@ -0,0 +1,12 @@
+function sumOddNum(arrNum) {
+  var sum = 0;
+  for (i = 0; i < arrNum.length; i++) {
+    a = parseInt(arrNum[i]);
+    if (a % 2 !== 0) {
+      sum += a;
+    }
+  }
+  return sum;
+}
+
+console.log(sumOddNum([1, 2, 3, 4, 5]));
