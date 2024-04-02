8927785bf57e7264ef467aa24bb4c0364a7951c7
최보윤 <144354615+Y0-0N63@users.noreply.github.com>
2024-04-02 22:50:39 +0900

    Update addOddNum.js

--- Week 06/addOddNum.js
+++ Week 06/addOddNum.js
@@ -3,6 +3,7 @@ function sumOddNum(arrNum) {
   for (i = 0; i < arrNum.length; i++) {
     // 배열의 요소들을 숫자로 바꿔주기
     a = parseInt(arrNum[i]);
+    // 홀수인 것만을 골라서 더해주기
     if (a % 2 !== 0) {
       sum += a;
     }
