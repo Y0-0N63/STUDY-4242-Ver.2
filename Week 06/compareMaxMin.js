7674af95680f505e87618b5902a07dce3f5a971b
최보윤 <144354615+Y0-0N63@users.noreply.github.com>
2024-04-04 01:00:22 +0900

    Update compareMaxMin.js

--- Week 06/compareMaxMin.js
+++ Week 06/compareMaxMin.js
@@ -8,36 +8,29 @@ function MaxOrMin(numbers) {
 
   // 배열의 요소들을 개별적인 요소로 구별한 후 숫자로 변환
   const inputNumStr = numbers.split(' ');
-
   const arrNum = [];
   for (let i = 0; i < inputNumStr.length; i++) {
     arrNum.push(parseInt(inputNumStr[i]));
     i++;
   }
 
-  // 최댓값 초기화
   let max = arrNum[0];
-  // 최솟값 초기화
   let min = arrNum[0];
 
   // 최댓값과 최솟값 찾기
   for (let i = 1; i < arrNum.length; i++) {
-    // 현재값과 최댓값 비교, 현재값이 최댓값보다 크면 최댓값으로 변경
     if (arrNum[i] > max) {
-      // 현재값과 최댓값 비교, 현재값이 최댓값보다 크면 최댓값으로 변경
       max = arrNum[i];
     }
 
     if (arrNum[i] < min) {
-      // 현재값과 최솟값 비교, 현재값보다 최솟값보다 작으면 최솟값 변경
       min = arrNum[i];
     }
   }
-  // 결과 반환
+
   console.log('최댓값은 ' + max + ' 최솟값은 ' + min);
 }
 
-// 임의의 숫자 5개 지정
 const inputNum = '58 234 12 45 2';
-//결과 출력
+//결과 출력하기
 console.log(MaxOrMin(inputNum));
